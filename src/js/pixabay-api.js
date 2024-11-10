7a87d6e899c158dfe5357756cc5430d6992144ae
Dmytro Koval <kdmytro793@gmail.com>
2024-11-10 22:21:34 +0200

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,21 @@
+import axios, { Axios } from "axios";
+
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "46814382-75c1b20cf6e14c25ef5bdd9a6";
 
-export default function fetchData(query) {
-    const params = new URLSearchParams({
-        key: API_KEY,
-        q: query,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true
+export default async function fetchData(query, page) {
+    const { data } = await axios(`${BASE_URL}`, {
+        params: {
+            key: API_KEY,
+            q: query,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: true,
+            page,
+            per_page: 15
+        }
     })
-
-    return fetch(`${BASE_URL}?${params}`)
-        .then(response => {
-        if (!response.ok) { 
-            throw new Error(response.statusText)
-            }
-            
-            return response.json();
-    })
+    return data;  
 }
 
 
--- src/main.js
+++ src/main.js
@@ -8,15 +8,22 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const form = document.querySelector(".form-search");
 const gallery = document.querySelector(".js-gallery");
 const loader = document.querySelector(".loader");
-let lightbox;
+const loadMore = document.querySelector(".btn-load-more")
 
 form.addEventListener("submit", querySearch);
+loadMore.addEventListener("click", onLoadMore);
+
+let lightbox;
+let currentPage = 1;
+const perPage = 15;
+const inputVal = "";
 
 function querySearch(event) {
     event.preventDefault();
     loader.style.display = "block";
-
     const inputVal = event.target.elements.query.value.trim();
+    currentPage = 1;
+    gallery.innerHTML = "";
 
     if (inputVal === "") {
           iziToast.show({
@@ -26,8 +33,8 @@ function querySearch(event) {
             color: "red"
           })
         loader.style.display = "none";
-
-    } else {
+        return;
+    }
 
     fetchData(inputVal)
         .then((response) => {
@@ -38,29 +45,66 @@ function querySearch(event) {
             position: "topRight",
             color: "red"
                 })
+                return;
             }
             
-            const markup = createMarkup(response.hits);
-            gallery.innerHTML = markup;
+            gallery.innerHTML = createMarkup(response.hits);
             loader.style.display = "none";
-            
+            loadMore.classList.remove("visually-hidden");
+
             if (lightbox) {
                 lightbox.refresh();
             } else {
                 lightbox = new SimpleLightbox(".gallery a"); 
-            }
-            
-            console.log(response.hits.length);
+            }            
         })
         .catch(error => {
             console.log(error);
-            
+
             iziToast.show({
                 title: "Error",
-                message:  " Sorry, there are no images matching your search query. Please try again!",
+                message:  "Sorry, there are no images matching your search query. Please try again!",
                 position: "topRight",
                 color: "red"
             })
         })
+}
+
+async function onLoadMore() {
+    currentPage++;
+    loadMore.disabled = true;
+    loadMore.style.display = "none";
+    loader.style.display = "block";
+
+    try {
+        const data = await fetchData(inputVal, currentPage);
+        gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits));
+
+       let totalPages = Math.ceil(data.totalHits / perPage);
+
+        if (data.hits.length === 0 || currentPage >= totalPages) {
+            loadMore.classList.add("visually-hidden");
+            iziToast.show({
+                message: "We're sorry, but you've reached the end of search results.",
+                position: "topRight",
+                color: "blue"
+            })
+        }
+        const card = document.querySelector(".gallery");
+        const cardHeight = card.getBoundingClientRect().height;
+        
+        window.scrollBy({
+            left: 0,
+            top: cardHeight * 2,
+            behavior: "smooth"
+        })
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loader.style.display = "none";
+        loadMore.style.display = "block";
+        loadMore.disabled = false;
     }
 }
+
+
